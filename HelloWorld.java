9976f64f50f3afa16f915ad81ef0f734d1a8b88e
Albert Attard <albertattard@gmail.com>
2021-12-22 10:46:17 +0100

    Generalize messages

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(final String[] args) {
-        System.out.println("Hello World!!");
+        Log.printMessage("Hello World!!");
+    }
+}
+
+class Log {
+    public static void printMessage(final String message) {
+        System.out.printf("[%tF %<tT] %s%n", System.currentTimeMillis(), message);
     }
 }
